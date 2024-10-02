cdc06c24f5735489f58a5619d39c276ecbdd72c6
emtri <emtric.cn@gmail.com>
2024-10-02 15:36:41 +0800

    Initial commit 实现了基本的配置，并添加上了element-ui-plus的基本组件和图标

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import {Document, Filter, Setting} from "@element-plus/icons-vue";
+
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+
+  </div>
+<!--  <el-icon style="vertical-align: middle"> Search </el-icon>-->
+  <el-icon><document/></el-icon>
+  <el-icon><Filter/></el-icon>
+  <HelloWorld msg="Vite + Vue" />
+  <el-button type="primary">我是 ElButton</el-button>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import {defineConfig} from 'vite'
+import vue from '@vitejs/plugin-vue'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        vue(),
+        AutoImport({
+            resolvers: [
+                ElementPlusResolver(),
+                IconsResolver({
+                    prefix: 'Icon',
+                }),
+            ],
+        }),
+        Components({
+            resolvers: [
+                ElementPlusResolver(),
+                IconsResolver({
+                    enabledCollections: ['ep'],
+                }),
+            ],
+        }),
+        Icons({
+            autoInstall: true,
+        })
+    ],
+})
+
